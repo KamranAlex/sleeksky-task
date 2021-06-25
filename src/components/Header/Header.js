8e3ad7910b254fcd19aa5fcb128034ec522ed6e0
KamranAlex <kamrana610@gmail.com>
2021-06-25 18:49:31 +0600

    header clock implement

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Logo from '../Logo/Logo';
 import './Header.css';
 
 const Header = () => {
+  const [date, setDate] = useState(new Date());
+  useEffect(() => {
+    var timerID = setInterval(() => updateTime(), 1000);
+    return () => {
+      clearInterval(timerID);
+    };
+  });
+  const updateTime = (params) => {
+    setDate(new Date());
+  };
+
+  const formatedDate = date.toLocaleTimeString('en-US', {
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric'
+  });
   return (
     <div className='header'>
       <Logo></Logo>
       <p className='time'>
-        Current Time is <span> 00: 00 : 00</span>{' '}
+        Current Time is <span>{formatedDate}</span>{' '}
       </p>
     </div>
   );
